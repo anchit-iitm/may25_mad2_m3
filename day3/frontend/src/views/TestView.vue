699bc86f5a4b52e8ef63c831a4d1788609fa5469
anchit mandal <21f1000692@ds.study.iitm.ac.in>
2025-07-08 11:03:44 +0530

    day 3 	modified:   .gitignore 	new file:   day3/backend/app.py 	new file:   day3/backend/config.py 	new file:   day3/backend/db_init.py 	new file:   day3/backend/models.py 	new file:   day3/backend/requirements.txt 	new file:   day3/backend/routes/auth.py 	new file:   day3/backend/routes/test.py 	new file:   day3/frontend/README.md 	new file:   day3/frontend/babel.config.js 	new file:   day3/frontend/jsconfig.json 	new file:   day3/frontend/package-lock.json 	new file:   day3/frontend/package.json 	new file:   day3/frontend/public/favicon.ico 	new file:   day3/frontend/public/index.html 	new file:   day3/frontend/src/App.vue 	new file:   day3/frontend/src/assets/logo.png 	new file:   day3/frontend/src/components/HelloWorld.vue 	new file:   day3/frontend/src/main.js 	new file:   day3/frontend/src/router/index.js 	new file:   day3/frontend/src/views/AboutView.vue 	new file:   day3/frontend/src/views/HomeView.vue 	new file:   day3/frontend/src/views/TestView.vue 	new file:   day3/frontend/vue.config.js 	new file:   day3/vue-project/README.md 	new file:   day3/vue-project/index.html 	new file:   day3/vue-project/jsconfig.json 	new file:   day3/vue-project/package-lock.json 	new file:   day3/vue-project/package.json 	new file:   day3/vue-project/public/favicon.ico 	new file:   day3/vue-project/src/App.vue 	new file:   day3/vue-project/src/assets/base.css 	new file:   day3/vue-project/src/assets/logo.svg 	new file:   day3/vue-project/src/assets/main.css 	new file:   day3/vue-project/src/components/HelloWorld.vue 	new file:   day3/vue-project/src/components/TheWelcome.vue 	new file:   day3/vue-project/src/components/WelcomeItem.vue 	new file:   day3/vue-project/src/components/icons/IconCommunity.vue 	new file:   day3/vue-project/src/components/icons/IconDocumentation.vue 	new file:   day3/vue-project/src/components/icons/IconEcosystem.vue 	new file:   day3/vue-project/src/components/icons/IconSupport.vue 	new file:   day3/vue-project/src/components/icons/IconTooling.vue 	new file:   day3/vue-project/src/main.js 	new file:   day3/vue-project/src/router/index.js 	new file:   day3/vue-project/src/views/AboutView.vue 	new file:   day3/vue-project/src/views/HomeView.vue 	new file:   day3/vue-project/vite.config.js

---
+++ day3/frontend/src/views/TestView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="test-page">
+        <h1>Test Page</h1>
+        <p>This is a test page to demonstrate Vue.js functionality.</p>
+        <p>Feel free to modify this page as needed.</p>
+        <div class="testPageField">
+            <label for="testPageTextField">Data: </label>
+            <input type="text" placeholder="Enter something here..." id="testPageTextField" v-model="this.testData"/>
+            <button v-on:click="this.printData()">print it in the console</button>
+        </div>
+        <br>
+        var: {{ this.testData }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestView',
+    data() {
+        return {
+            testData: '',
+        };
+    },
+    methods: {
+        printData() {
+            console.log(this.testData);
+        },
+    },
+};
+</script>
+
+<style>
+
+</style>
